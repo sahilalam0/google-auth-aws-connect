8aab10e329ac30c477990f6669939b1de9a6a8ab
sahilalam0 <sahil.mansoor@anywhere.co>
2024-04-16 21:38:01 +0530

    removed unnecessary comment

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import opn from 'open';
 import destroyer from 'server-destroy';
 import { google } from 'googleapis';
 import 'dotenv/config';
-// fullcreative workspace
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
